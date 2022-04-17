64f669c1e5a1b8983ec036ca2860c0c541996984
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-16 23:46:18 -0500

    Conexión como inyectable.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Db } from 'mongodb';
 import config from './config';
 
 @Injectable()
@@ -7,11 +8,16 @@ export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject('MONGO') private db: Db,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
+
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+  getTasks() {
+    return this.db;
   }
 }
